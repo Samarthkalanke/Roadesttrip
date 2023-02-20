4857508930f43f0ef96dae90b272f75a4c18f0b8
Samarth <samarthkalanke@gmail.com>
2023-02-20 08:26:44 -0800

    frsfws

--- api/covid.py
+++ api/covid.py
@@ -125,19 +125,3 @@ if __name__ == "__main__":
         print(key, value)
         
     print("-"*30)
-
-    
-import requests
-
-url = "https://car-data.p.rapidapi.com/cars"
-
-querystring = {"limit":"10","page":"0"}
-
-headers = {
-	"X-RapidAPI-Key": "8e39206459msh63119b14325b472p1a320bjsnac696cb1d4af",
-	"X-RapidAPI-Host": "car-data.p.rapidapi.com"
-}
-
-response = requests.request("GET", url, headers=headers, params=querystring)
-
-print(response.text)
